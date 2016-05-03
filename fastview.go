742ced4c661787e179d7443eae90fcd2fd4e7c56
Ferenc Engard <ferenc.engard@ge.com>
2016-05-03 22:52:19 +0200

    initial alpha

--- fastview.go
+++ fastview.go
@@ -3,12 +3,14 @@ package main
 import (
     "fmt"
     "net/http"
-    "time"
+    //~ "time"
     //~ "github.com/tonnerre/golang-pretty"
     "path"
     "strings"
     "os"
     "io/ioutil"
+    "os/exec"
+    "strconv"
     //~ "log"
 )
 
@@ -17,8 +19,9 @@ import (
 //~ )
 
 const (
-    THUMB_SMALL = iota
-    THUMB_MED = iota
+    NOTHING = iota
+    DIRECTORY = iota
+    REGULAR = iota
 )
 
 type Config struct {
@@ -31,6 +34,8 @@ var localDirsMap = map[string]string {
     "/pictures": "/home/fery/Pictures",
 }
 
+var allowedSizes=[...]int {640,1600}
+
 //~ var localDirsMapReverse = new map[string]string
 
 var config = Config {
@@ -38,25 +43,15 @@ var config = Config {
     "/home/fery/magan/fastview/fastview/data/cache",
 }
 
-
-
 type Image struct {
     // full URL path (including the source identifier, e.g. localImageBaseDir)
     contentURL string
     //lastMod time.Time
-    cache map[int]Cache
+    //cache map[int]Cache
 }
 
 func (i Image) x(){
 
-}
-
-type Cache struct {
-    contentPath string
-    lastMod time.Time
-    // the longer dimension
-    resolution int
-    cachePath string
 }
 
 // Map of URL Path -> Image
@@ -66,6 +61,18 @@ func sendErr(w http.ResponseWriter, error string) {
     fmt.Fprint(w, error);
 }
 
+func inspectFile(name string) int {
+    fstat, err := os.Stat(name)
+	if err != nil {
+        return NOTHING
+    }
+    if fstat.IsDir() {
+        return DIRECTORY
+    } else {
+        return REGULAR
+    }
+}
+
 // ContentProvider & implementers
 
 type ContentProvider interface{
@@ -73,6 +80,7 @@ type ContentProvider interface{
     Path2Url(cpPath string) string
     // Convert public (fastview) URL (only path part) to ContentProvider-specific path
     Url2Path(url string) string
+    mkCacheFilename(abspath, sizeStr string) string
 }
 
 type LocalFilesystemProvider struct {
@@ -108,8 +116,7 @@ func (cp *LocalFilesystemProvider) serveDir(urlPath string, fsPath string, w htt
     dirnames := make([]string, 0, len(files))
 	for _, file := range files {
         url := fmt.Sprintf("\"%v\"", path.Join(urlPath, file.Name()))
-        fstat, _ := os.Stat(path.Join(fsPath, file.Name()))
-        if fstat.IsDir() {
+        if inspectFile(path.Join(fsPath, file.Name()))==DIRECTORY {
             dirnames=append(dirnames, url)
         } else {
             filenames=append(filenames, url)
@@ -121,23 +128,107 @@ func (cp *LocalFilesystemProvider) serveDir(urlPath string, fsPath string, w htt
 }
 
 func (cp *LocalFilesystemProvider) serveSingleImage(abspath string, w http.ResponseWriter, r *http.Request) {
-    size := r.FormValue("size")
-    if len(size)==0 || size=="full" {
+    sizeStr := r.FormValue("size")
+    size, err := strconv.Atoi(sizeStr)
+    if err!=nil || len(sizeStr)==0 || sizeStr=="full" || size>allowedSizes[len(allowedSizes)-1] {
         http.ServeFile(w, r, abspath)
         return
     }
-    if size=="med" {
-        fullpath := path.Join(config.cacheDir, cp.Path2Url(abspath))
-        dirname := path.Dir(fullpath)
-        if err := os.MkdirAll(dirname, 0644); err != nil {
-            fmt.Fprintf(os.Stderr, "error creating cache directory: %v", )
-            // fallback
-            http.ServeFile(w, r, abspath)
-            return
+    for _, val:=range allowedSizes {
+        if size<=val {
+            size=val
+            break;
         }
+    }
+    sizeStr=strconv.Itoa(size)
+    fmt.Printf("serving from cache, size: %v, %v\n", size, sizeStr)
+    cachepath := cp.mkCacheFilename(abspath, sizeStr)
+    if !mkThumbnail(cachepath, abspath, size, true) {
+        // fallback
+        http.ServeFile(w, r, abspath)
+        return
+    }
+    http.ServeFile(w, r, cachepath)
+}
 
+func mkThumbnail(cachepath, abspath string, size int, sync bool) bool {
+    if inspectFile(cachepath)==REGULAR {
+        cacheStat, _ := os.Stat(cachepath)
+        origStat, _ := os.Stat(abspath)
+        if  cacheStat.ModTime().After(origStat.ModTime()) {
+            // we already have it
+            return true
+        }
+    }
+    var callback chan bool
+    if sync {
+        callback=make(chan bool)
+    }
+    task := RescaleTask{abspath, cachepath, size, callback}
+    go func () {urgentRescaleTasks <-task}()
+    if sync {
+        // this will synchronize
+        return <-callback
+    }
+    return true
+}
+
+func (cp *LocalFilesystemProvider) mkCacheFilename(abspath, sizeStr string) string {
+    return path.Join(config.cacheDir, sizeStr, cp.Path2Url(abspath))
+}
+
+type RescaleTask struct {
+    ImagePath string
+    CachePath string
+    Size int
+    // channel to indicate the result of conversion (true: success, false: failure)
+    Callback chan bool
+}
+
+var urgentRescaleTasks chan RescaleTask = make(chan RescaleTask)
+
+func resizeImage(dest, src string, size int) bool {
+	var args = []string{
+		"-s", fmt.Sprintf("%v", size),
+		"-o", dest,
+		src,
+	}
+	var cmd *exec.Cmd
+	path, _ := exec.LookPath("vipsthumbnail")
+	cmd = exec.Command(path, args...)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Error while running '%v': %v", args, err)
+	}
+	return err==nil
+}
+
+func thumbnailGenerator() {
+    for task := range urgentRescaleTasks {
+        fmt.Printf("Rescale task: %v\n", task)
+        dirname := path.Dir(task.CachePath)
+        result := true
+        if err := os.MkdirAll(dirname, 0755); err != nil {
+            fmt.Fprintf(os.Stderr, "error creating cache directory: %v - error: %v\n", dirname, err)
+            result=false
+        } else {
+            //~ var size int
+            //~ switch task.ThumbSize {
+            //~ case THUMB_SMALL: size=320
+            //~ case THUMB_MED: size=800
+            //~ default: result=false;
+            //~ }
+            if result {
+                result = resizeImage(task.CachePath, task.ImagePath, task.Size)
+            }
+        }
+        if task.Callback!=nil {
+            task.Callback <- result
+            close(task.Callback)
+        }
     }
 }
+
 
 // Serves images and directories from the local filesystem.
 //
@@ -150,32 +241,22 @@ func (cp *LocalFilesystemProvider) serveLocal(w http.ResponseWriter, r *http.Req
 
     cleanedPath := path.Clean(r.URL.Path)
     abspath := cp.Url2Path(cleanedPath)
-    fmt.Printf("cleanedPath: %v\nfs path: %v\n", cleanedPath, abspath)
+    fmt.Printf("serveLocal() cleanedPath: %v, fs path: %v\n", cleanedPath, abspath)
     if len(abspath)==0 {
         fmt.Fprintf(w, "invalid URL")
         return
     }
 
-    fstat, err := os.Stat(abspath)
-	if err != nil {
-        fmt.Fprintf(w, "Bad path")
-        return
-    }
-    if fstat.IsDir() {
-        cp.serveDir(cleanedPath, abspath, w, r)
-    } else {
-        cp.serveSingleImage(abspath, w, r)
+    switch inspectFile(abspath) {
+    case NOTHING: fmt.Fprintf(w, "Bad path")
+    case DIRECTORY: cp.serveDir(cleanedPath, abspath, w, r)
+    case REGULAR: cp.serveSingleImage(abspath, w, r)
     }
 }
 
-//~ func myFileServer(w http.ResponseWriter, r *http.Request) {
-    //~ handler.ServeHTTP(w, r)
-//~ }
-
-//~ var handler http.Handler =
-
 func main() {
     fmt.Printf("Starting\n")
+    go thumbnailGenerator()
     http.Handle("/site/", http.FileServer(http.Dir(".")))
 
     for urlbase, dir := range localDirsMap {
--- site/scripts/fastview.js
+++ site/scripts/fastview.js
@@ -1,78 +1,192 @@
- "use strict";
+"use strict";
+
+var thumbnailSize=640;
+var fullscreenSize=1600;
+
+var thumbnailStyleSize=320;
 
 $(document).ready(function() {
-    console.log("initFastview()");
-
+    console.log("document.ready()");
     $("#testbtn").click(function() {
        var thumbnails=$("#thumbnails");
         thumbnails.empty();
-    })
+    });
 
-    $(document).on("click", "#directories a", function(e) {
-        $.bbq.pushState({dir: $(this).attr('href')});
-        return false;
+    $("#thumb-size").on("keydown", function(e) {
+        if (e.keyCode == 13) {
+            var newVal=parseInt($(this).val());
+            if (newVal>=96 && newVal<=fullscreenSize) {
+                thumbnailStyleSize=newVal;
+                $.bbq.removeState("index");
+                $(window).trigger( 'hashchange' );
+            }
+        }
     });
 
     $(window).bind( 'hashchange', function(e) {
+        console.log("hashchange "+window.location.href);
         var dir=$.bbq.getState("dir");
         if (dir) {
-            loadDir(dir);
+            selectPage("thumbnails");
+            loadDir(dir, $.bbq.getState("index"));
+            return;
+        }
+        var image=$.bbq.getState("image");
+        if (image) {
+            if ($("#thumbnails").children().length==0) {
+                // presumably opened a direct link to an image -- must load the thumbnails...
+                loadDir(image.substring(0,image.lastIndexOf("/")));
+            }
+            selectPage("fullscreen");
+            showImage(image);
+            return;
         }
     });
 
     if (window.location.href.indexOf("#")<0) {
-        window.location+="#dir=/pictures/";
+        //~ window.location+="#dir=/pictures/";
+        $.bbq.pushState({"dir": "/pictures"});
     }
 
     $(window).trigger( 'hashchange' );
 });
 
+// single page app support
+// @param page "fullscreen" or "thumbnails"
+function selectPage(page) {
+    $("#fullscreen-page").hide();
+    $("#thumbnails-page").hide();
+
+    $("#"+page+"-page").show();
+
+    // direction: -1 or +1
+    function nextImage(direction) {
+        var newIndex=parseInt($.bbq.getState("index"), 10)+direction;
+        if (newIndex<0) return undefined;
+        var imgEl=$("#thumbnails img:eq("+newIndex+")");
+        if (imgEl.length==0) return undefined;
+        imgEl.trigger("click");
+    }
+
+    function handleFullscreenKeys(e) {
+        if (e.keyCode == 27) { // escape key maps to keycode `27`
+            var image=$.bbq.getState("image");
+            var dir=image.substring(0,image.lastIndexOf("/"));
+            window.location.href="#index="+encodeURIComponent($.bbq.getState("index"))+"&dir="+encodeURIComponent(dir);
+            return false;
+        } else if (e.keyCode == 37 || e.keyCode == 39) { // 37: left, 39: right
+            nextImage(e.keyCode-38);
+            return false;
+        }
+        return true;
+    }
+
+    function handleFullscreenWheel(e) {
+        var delta=e.originalEvent.deltaY;
+        if (delta==0) return;
+        nextImage(delta>0 ? 1 : -1);
+    }
+    $(document).off("keyup.fastview");
+    $(document).off("wheel.fastview");
+    if (page==="fullscreen") {
+        $(document).on("keyup.fastview", handleFullscreenKeys);
+        $(document).on("wheel.fastview", handleFullscreenWheel);
+    }
+}
+
 function loadDir(dir) {
+    var thumbnails=$("#thumbnails");
+    var dirs=$("#directories");
+    thumbnails.empty();
+    dirs.empty();
+
     $.ajax({
       url: dir,
     })
     .done(function( data ) {
-        console.log( "Data loaded: ", data);
+        //~ console.log( "Data loaded: ", data);
         $("#current-dir-name").text(dir);
         var list=JSON.parse(data);
         var imagelist=list.images;
         if (imagelist) {
-            var thumbnails=$("#thumbnails");
-            thumbnails.empty();
-            imagelist.forEach(function(path) {
-                console.log("image path:", path);
+            imagelist.forEach(function(path, i) {
+                //~ console.log("image path:", path);
                 var img=$('<img />', {
-                    src: path,
+                    src: path+"?size="+(thumbnailStyleSize>thumbnailSize ? fullscreenSize : thumbnailSize),
                     alt: path.substring(path.lastIndexOf("/")+1),
                     class: "thumbnail"
                 });
                 img.css("padding", "10px");
+                img.css("max-height", thumbnailStyleSize+"px");
+                img.css("max-width", thumbnailStyleSize+"px");
                 img.click(function() {
-                    window.location.href=path;
+                    window.location.href="#size="+fullscreenSize+"&index="+i+"&image="+encodeURIComponent(path);
                 });
-                var span=$('<span/>');
-                span.append(img);
-                thumbnails.append(span);
+                //~ var span=$('<span/>');
+                //~ span.append(img);
+                thumbnails.append(img);
             });
+        }
+
+        function addDirLink(path, text) {
+            console.log("addDirLink ", path, text);
+            var link=$('<a />', {
+                href: "#dir="+encodeURIComponent(path),
+            });
+            link.text(text ? text : path);
+            dirs.append(link);
+            dirs.append($('<br/>'));
         }
 
         var dirlist=list.dirs;
-        if (dirlist) {
-            var dirs=$("#directories");
-            dirs.empty();
-            dirlist.forEach(function(path) {
-                console.log("dir path:", path);
-                var link=$('<a />', {
-                    href: path,
-                });
-                link.text(path);
-                //~ var span=$('<span/>', {
-                //~ });
-                //~ span.append(img);
-                dirs.append(link);
-                dirs.append($('<br/>'));
-            });
+        if (dir.lastIndexOf("/")>0) {
+            var parent=dir.substring(0,dir.lastIndexOf("/"));
+            if (parent.length>1) {
+                addDirLink(parent, "..")
+            }
         }
+        if (dirlist) {
+            // forEach unfortunately calls its param with 2 arguments :(
+            dirlist.forEach(function(path) {addDirLink(path);});
+        }
+    });
 
+    if (parseInt($.bbq.getState("index"))>0) {
+        var index=parseInt($.bbq.getState("index"), 10);
+        var imgEl=$("#thumbnails img:eq("+(index)+")");
+
+        var scroll=function() {
+            var imgEl=$("#thumbnails img:eq("+(index)+")");
+            if (imgEl.offset()==undefined) {
+                console.log("wait for scroll...");
+                window.requestAnimationFrame(scroll);
+                return;
+            }
+            console.log("scroll!");
+            $('html, body').animate({
+                scrollTop: imgEl.offset().top
+            }, 500);
+        }
+        scroll();
+    }
+}
+
+function showImage(path) {
+    var fullscreen=$("#fullscreen-page");
+    fullscreen.empty();
+    console.log("showImage("+path+")");
+
+    var img=$('<img />', {
+        src: path+"?size="+$.bbq.getState("size"),
+        alt: path.substring(path.lastIndexOf("/")+1),
+    });
+    // this is ok for not-so-narrow browser sizes; otherwise should do something like in
+    // http://stackoverflow.com/questions/20590239/maintain-aspect-ratio-of-div-but-fill-screen-width-and-height-in-css
+    img.photoResize({bottomSpacing: 0});
+    img.on("load", function() { // prevent flickering
+        fullscreen.append(img);
+    });
+    img.click(function() {
+        window.location.href=path;
     });
 }
